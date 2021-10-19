cbb6045bd4047c516cc13c387ddedc7991a38bc6
Your Name <shelbyfayemo@gmail.com>
2021-10-19 17:22:52 -0400

    added routes

--- public/assets/js/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const app = express();
-const PORT = 3001;
-
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", htmlRoutes);
+app.use("/", apiRoutes);
+
+app.listen(PORT, () => {
+  console.log("`API server now on port {PORT}");
+});

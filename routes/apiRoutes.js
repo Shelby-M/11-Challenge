18a2b0b67fc54343f9d76e206cf012a9a97aea8e
Your Name <shelbyfayemo@gmail.com>
2021-10-23 04:42:50 -0400

    worked with tutor to write routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+const db = require("../db/db.json");
+const fs = require("fs");
+
+router.get("/notes", (req, res) => {
+  const notes = fs.readFileSync("../db/db.json", "utf-8");
+  res.json(notes);
+});
+
+module.exports = router;

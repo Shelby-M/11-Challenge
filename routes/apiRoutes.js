a56d1a00a6248fbfeb40c7ced465bc5d316c662a
Your Name <shelbyfayemo@gmail.com>
2021-10-25 12:20:51 -0400

    error in starter code js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const db = require("../db/db.json");
 const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
-  const notes = fs.readFileSync("../db/db.json", "utf-8");
+  const notes = fs.readFileSync(path.join(__dirname, "../db/db.json"));
   res.json(notes);
 });
 
